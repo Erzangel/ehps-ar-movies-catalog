eb92793deb6a4d407423613fe2d14e1393316d1b
XeKkoW <gabriel.vigne@edu.ece.fr>
2023-01-12 15:07:23 +0100

    création home page

---
+++ src/pages/Home.js
@@ -0,0 +1,32 @@
+import { StatusBar } from "react-native";
+import { Text, View } from "react-native";
+import { StyleSheet } from "react-native";
+
+const Home = () => {
+    return (
+        
+<View>
+<Text style = {styles.welcome}> Mirage</Text>
+<StatusBar style = "auto"/>
+</View>
+    );
+};
+
+export default Home;
+
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    welcome: {  
+      fontSize: 20,  
+      textAlign: 'center',  
+      margin: 10,  
+    },
+  });
+
