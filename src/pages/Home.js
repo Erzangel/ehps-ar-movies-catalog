781cca710c2245ce63e2c24eeac1bd2e723b1505
XeKkoW <gabriel.vigne@edu.ece.fr>
2023-01-17 13:59:19 +0100

    home page fini

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,13 @@
 
 import { StatusBar } from "react-native";
-import { Text, View } from "react-native";
+import { Text, View, FlatList, SafeAreaView } from "react-native";
 import { StyleSheet } from "react-native";
 import NavigationBar from "../components/BottomTabNavigator";
 import { useNavigation } from '@react-navigation/native';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { BackHandler } from 'react-native';
+import { ScrollView } from "react-native-gesture-handler";
 
-
-const Home = () => {
   /*
   useEffect(() => {
     const backHandler = BackHandler.addEventListener(
@@ -20,41 +19,127 @@ const Home = () => {
     );
     return () => backHandler.remove();
   }, []);
+  
+   
 */
 
+const Home = () => {
+  const [nb, setNb] = useState(5);
 
-  return (
-    <View style={styles.container}>
+
+const whiteSquare = () => <View style={styles.square} />
+const ItemSeparator = () => <View style={styles.separator} />;
+
+return (
+  
+  <View style={styles.container}>
+    <View style={styles.scrollContainer}>
+    <ScrollView style={styles.scrollContainer} showsVerticalScrollIndicator={false}>
+    <View style={styles.welcomeContainer}>
       <Text style={styles.welcome}>Mirage</Text>
-      <StatusBar style="auto"/>
-      <View style={styles.navigationBarContainer}>
-        <NavigationBar/>
-      </View>
     </View>
-  );
+    <View style={styles.bigSquare} />
+
+    <View style={styles.list}>
+      <Text style={styles.new}> Nouveautées </Text>
+<SafeAreaView>
+    <FlatList
+      data={Array(nb)}
+      renderItem={() => whiteSquare()}
+      keyExtractor={(item, index) => index.toString()}
+      horizontal={true}
+      ItemSeparatorComponent={ItemSeparator}
+      showsHorizontalScrollIndicator={false}
+    />
+    </SafeAreaView>
+    </View>
+
+    <View style={styles.list}>
+      <Text style={styles.new}> Tendances actuelles </Text>
+<SafeAreaView>
+    <FlatList
+      data={Array(nb)}
+      renderItem={() => whiteSquare()}
+      keyExtractor={(item, index) => index.toString()}
+      horizontal={true}
+      ItemSeparatorComponent={ItemSeparator}
+      showsHorizontalScrollIndicator={false}
+    />
+    </SafeAreaView>
+    </View>
+
+
+
+    <StatusBar style="auto"/>
+   
+    </ScrollView>
+    </View>
+ 
+  <View style={styles.navigationBarContainer}>
+      <NavigationBar/>
+    </View>
+  </View>
+ 
+
+);
 };
 
 export default Home;
 
 
-
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    backgroundColor: 'black',  
+  },
+  welcomeContainer: {
+    textAlign: 'center',
+    alignSelf: 'center',
+
   },
   welcome: {  
-    fontSize: 20,  
+    fontSize: 30,  
     textAlign: 'center',  
     margin: 10,  
+    color: 'white'
   },
   navigationBarContainer: {
     position: 'absolute',
     left: 0,
     right: 0,
     bottom: 0,
+    zIndex: 1,
   },
+  bigSquare: {
+    alignSelf:'center',
+    width: 350,
+    height: 200,
+    marginBottom: 20,
+    backgroundColor: 'white',
+  },
+  new: {
+    fontSize: 15,  
+    height : 30, 
+    color: 'white'
+  },
+  square: {
+    width: 110, 
+    height: 150, 
+    marginLeft: 5,
+    backgroundColor: 'white',
+  },
+  separator: {
+    width: 5,
+  },
+  scrollContainer: {
+    flex: 1,
+    paddingBottom: 50, //taille de la barre de navigation
+  },
+  list: {
+    marginBottom: 20,
+  }
 });
+
 
